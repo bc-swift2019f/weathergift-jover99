97267359f060cceee986fde3455af1aa7215a7ff
Richard Jove <jover@bc.edu>
2019-10-09 21:30:06 -0400

    Initial Commit

---
+++ WeatherGift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherGift
+//
+//  Created by Richard Jove on 10/9/19.
+//  Copyright © 2019 Richard Jove. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
